5583b6ee21807334630c652e64d027c18d538547
hp <riteshgharti121232@gmail.com>
2025-06-21 18:54:50 +0530

    fix issue

--- app/backend/dist/lib/prisma.js
+++ app/backend/dist/lib/prisma.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
+// src/lib/prisma.ts
 const client_1 = require("@prisma/client");
-const globalForPrisma = globalThis;
-exports.prisma = globalForPrisma.prisma ??
-    new client_1.PrismaClient({
-        log: ["error", "warn"], // none 
-    });
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = exports.prisma;
+exports.prisma = new client_1.PrismaClient();
--- app/backend/src/lib/prisma.ts
+++ app/backend/src/lib/prisma.ts
@@ -1,22 +1,4 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
-
-const prismaClient =
-  globalForPrisma.prisma ??
-  new (PrismaClient as any)({
-    log: ["error", "warn"],
-    __internal: {
-      engine: {
-        enablePreparedStatements: false,
-      },
-    },
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prismaClient;
-}
-
-export const prisma = prismaClient;
+export const prisma = new PrismaClient();
