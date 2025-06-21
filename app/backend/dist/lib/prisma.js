28fd9aa9d40a8c86aa85ad35e20f7266ff3f2865
hp <riteshgharti121232@gmail.com>
2025-06-21 18:44:17 +0530

    checking new error

--- app/backend/dist/lib/prisma.js
+++ app/backend/dist/lib/prisma.js
@@ -5,7 +5,7 @@ const client_1 = require("@prisma/client");
 const globalForPrisma = globalThis;
 exports.prisma = globalForPrisma.prisma ??
     new client_1.PrismaClient({
-        log: ["error", "warn"],
+        log: ["error", "warn"], // none 
     });
 if (process.env.NODE_ENV !== "production")
     globalForPrisma.prisma = exports.prisma;
