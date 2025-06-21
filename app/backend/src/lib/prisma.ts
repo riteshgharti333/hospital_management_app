7eaf8bca5ba16c3dd27ce9f59834c976234b7f2f
hp <riteshgharti121232@gmail.com>
2025-06-21 18:52:38 +0530

    fix issue

--- app/backend/src/lib/prisma.ts
+++ app/backend/src/lib/prisma.ts
@@ -1,13 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
-export const prisma =
+const prismaClient =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"], // none 
+  new (PrismaClient as any)({
+    log: ["error", "warn"],
+    __internal: {
+      engine: {
+        enablePreparedStatements: false,
+      },
+    },
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prismaClient;
+}
+
+export const prisma = prismaClient;
