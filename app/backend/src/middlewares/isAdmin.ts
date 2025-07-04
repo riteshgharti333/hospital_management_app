5146c89104fa732820422a92e4d60492ed68db2b
hp <riteshgharti121232@gmail.com>
2025-07-04 17:44:09 +0530

    checking render error

--- app/backend/src/middlewares/isAdmin.ts
+++ app/backend/src/middlewares/isAdmin.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import "express";
+// import "express";
 import { ErrorHandler } from "./errorHandler";
 import { StatusCodes } from "../constants/statusCodes";
 
