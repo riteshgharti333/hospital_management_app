88b41f87a9812f42d8dd5b743f258c991f7fe090
hp <riteshgharti121232@gmail.com>
2025-07-06 10:47:24 +0530

    checking git error

--- app/backend/dist/middlewares/isAdmin.js
+++ app/backend/dist/middlewares/isAdmin.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isAdmin = void 0;
-require("express");
+// import "express";
 const errorHandler_1 = require("./errorHandler");
 const statusCodes_1 = require("../constants/statusCodes");
 const isAdmin = (req, res, next) => {
