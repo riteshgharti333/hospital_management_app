ffe3003db4af17be5c0d57219d63c2783c2b7d5b
hp <riteshgharti121232@gmail.com>
2026-04-29 16:17:49 +0530

    fix: remove backend dist from git

--- app/backend/dist/lib/paginationConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAGINATION_CONFIG = void 0;
-exports.PAGINATION_CONFIG = {
-    DEFAULT_LIMIT: 50,
-    MAX_LIMIT: 100,
-};
